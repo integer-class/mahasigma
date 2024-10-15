fd085e2455b84713c9557e63f177098e34a41782
Davis Maulana Hermanto <davismaulana05@gmail.com>
2024-10-15 15:14:15 +0700

    fix home page

--- ohmyglow/lib/main.dart
+++ ohmyglow/lib/main.dart
@@ -33,95 +33,79 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0; // Index of the currently selected item
 
-  void _incrementCounter() {
+  // List of widgets for each page
+  final List<Widget> _pages = [
+    Center(child: Text('Home Page')),
+    Center(child: Text('Activity Page')),
+    Center(child: Text('Analytics Page')),
+    Center(child: Text('Profile Page')),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index; // Update the selected index
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: HomePage(),
+      bottomNavigationBar: Container(
+        color: Colors.green[100], // Set background color
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '', // Empty label for Home
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_time),
+              label: '', // Empty label for Activity
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox.shrink(), // Empty space for center button
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart),
+              label: '', // Empty label for Analytics
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: '', // Empty label for Profile
             ),
           ],
+          currentIndex: _selectedIndex, // Current selected index
+          selectedItemColor: Colors.black, // Color of the selected item
+          unselectedItemColor: Colors.grey, // Color of the unselected items
+          onTap: _onItemTapped, // Function called when an item is tapped
+          type: BottomNavigationBarType.fixed, // For fixed layout
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          // Action for the center button
+        },
+        backgroundColor: Colors.green, // Set button color
+        child: const Icon(Icons.face), // Center button icon
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
--- ohmyglow/lib/pages/home.dart
+++ ohmyglow/lib/pages/home.dart
@@ -17,120 +17,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0; // Index of the currently selected item
-
-  // List of widgets for each page
-  final List<Widget> _pages = [
-    Center(child: Text('Home Page')),
-    Center(child: Text('Activity Page')),
-    Center(child: Text('Analytics Page')),
-    Center(child: Text('Profile Page')),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index; // Update the selected index
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            cardDashboard,
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-              child: Text(
-                "How is Your Face Skin Today?",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFFD14D72)),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                "Skin Problem",
-                style: TextStyle(
-                  fontSize: 16,
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          cardDashboard,
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Text(
+              "How is Your Face Skin Today?",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 40,
                   fontWeight: FontWeight.bold,
-                ),
+                  color: Color(0xFFD14D72)),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Skin Problem",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Cardmenu(
-                      image: widget.items[0].image,
-                      title: widget.items[0].title),
-                  Cardmenu(
-                      image: widget.items[1].image,
-                      title: widget.items[1].title),
-                  Cardmenu(
-                      image: widget.items[2].image,
-                      title: widget.items[2].title),
-                ],
-              ),
-            )
-            // ListView.builder(
-            //   itemCount: widget.items.length,
-            //   scrollDirection: Axis.horizontal,
-            //   itemBuilder: (context, index) {
-            //     final item = widget.items[index];
-            //     return Cardmenu(image: item.image, title: item.title);
-            //   },
-            // )
-          ],
-        ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Cardmenu(
+                    image: widget.items[0].image, title: widget.items[0].title),
+                Cardmenu(
+                    image: widget.items[1].image, title: widget.items[1].title),
+                Cardmenu(
+                    image: widget.items[2].image, title: widget.items[2].title),
+              ],
+            ),
+          )
+          // ListView.builder(
+          //   itemCount: widget.items.length,
+          //   scrollDirection: Axis.horizontal,
+          //   itemBuilder: (context, index) {
+          //     final item = widget.items[index];
+          //     return Cardmenu(image: item.image, title: item.title);
+          //   },
+          // )
+        ],
       ),
-      bottomNavigationBar: Container(
-        color: Colors.green[100], // Set background color
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '', // Empty label for Home
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.access_time),
-              label: '', // Empty label for Activity
-            ),
-            BottomNavigationBarItem(
-              icon: SizedBox.shrink(), // Empty space for center button
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.show_chart),
-              label: '', // Empty label for Analytics
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: '', // Empty label for Profile
-            ),
-          ],
-          currentIndex: _selectedIndex, // Current selected index
-          selectedItemColor: Colors.black, // Color of the selected item
-          unselectedItemColor: Colors.grey, // Color of the unselected items
-          onTap: _onItemTapped, // Function called when an item is tapped
-          type: BottomNavigationBarType.fixed, // For fixed layout
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Action for the center button
-        },
-        backgroundColor: Colors.green, // Set button color
-        child: const Icon(Icons.face), // Center button icon
-      ),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.centerDocked, // Position the FAB
     );
   }
 }
